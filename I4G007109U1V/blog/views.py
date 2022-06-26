a911868d8a2a819d7c1a51f1f93ea15122be2e5e
AyoOlu1 <oluwayemivictor15@gmail.com>
2022-06-26 18:50:17 +0100

    fixed indentation in post_list.html

--- I4G007109U1V/blog/views.py
+++ I4G007109U1V/blog/views.py
@@ -1,5 +1,5 @@
 # Create your views here.
-from django.views.generic.list import ListView, 
+from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView 
 from django.views.generic.edit import CreateView, UpdateView
 from .models import Post
